233c60c8e22cd944d6cbca682fe2c768305b3041
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-12-19 13:46:06 +0530

    ng switch

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -6,44 +6,9 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  // names: string[] = ['Ajit','Abhishek', 'avinash'];
-  users = [
-    {
-      name: "ajit",
-      email: "ajit@123.com",
-      skill: "clang",
-      website:'www.google.com'
-    },
-    { name: "ajibvgt", 
-      email: "ajihgfhgt@123.com",  
-      skill: "clanhghjg",
-      website:'www.google.com'
-     },
-    { name: "ajithbghgv", 
-    email: "ajitghhgghgh@123.com", 
-    skill: "clang" ,
-    website:'www.google.com'},
-
-    { name: "ajvhgit", 
-    email: "ajitbbnb@123.com", 
-    skill: "clanbnbg" ,
-    website:'www.google.com'},
-  ];
-
-curentUser = null;
-isDisabled = true;
-  showAndEdit(indexNum){
-    this.isDisabled = true;
-    this.curentUser = this.users[indexNum];
-  }
-
-  setValue(event){
- const value = event.target.value;
- const keyName = event.target.name;
- this.curentUser[keyName]=value;
-  }
-
-  enableEditing() {
-    this.isDisabled = false;
+  // name ='Sagar';
+  currentTab='';
+  openTab(tabName=''){
+this.currentTab=tabName;
   }
 }
